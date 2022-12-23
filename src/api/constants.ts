d1fc342bd8bebdc62d92da85b0f2cde901bd7ed8
osipik.a <osipik.av@yandex.by>
2022-12-23 23:53:50 +0300

    fix criticism

---
+++ src/api/constants.ts
@@ -0,0 +1,2 @@
+export const PRODUCTS_NUMBER = 41;
+export const BASE_URL = 'https://api.escuelajs.co/api/v1/products'
--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import './styles.scss';
+import { PRODUCTS_NUMBER, BASE_URL } from './api/constants.ts';
 
-
-let products = document.querySelector(".products");
-let url = "https://api.escuelajs.co/api/v1/products";
+let products = document.querySelector('.products');
 let data;
-
-let numberOfProducts = 41;
 
 async function getProducts() {
   try {
-    let res = await fetch(url);
+    let res = await fetch(BASE_URL);
     data = await res.json();
     mkProducts();
   } catch (error) {
@@ -20,25 +17,25 @@ async function getProducts() {
 getProducts();
 
 function itemMaker(id) {
-  let productItem = document.createElement("div");
-  let productTitle = document.createElement("div");
-  let productPhoto = document.createElement("div");
-  let productImg = document.createElement("img");
-  let productPrice = document.createElement("div");
-  let productCur = document.createElement("span");
-  let productOptions = document.createElement("div");
-  let productDetails = document.createElement("button");
-  let productCart = document.createElement("button");
+  let productItem = document.createElement('div');
+  let productTitle = document.createElement('div');
+  let productPhoto = document.createElement('div');
+  let productImg = document.createElement('img');
+  let productPrice = document.createElement('div');
+  let productCur = document.createElement('span');
+  let productOptions = document.createElement('div');
+  let productDetails = document.createElement('button');
+  let productCart = document.createElement('button');
 
-  productItem.className = "products__item";
-  productTitle.className = "products__title";
-  productPhoto.className = "products__photo";
-  productImg.className = "products__img";
-  productPrice.className = "products__price";
-  productCur.className = "products__currency";
-  productOptions.className = "products__options";
-  productDetails.className = "products__details";
-  productCart.className = "products__cart";
+  productItem.className = 'products__item';
+  productTitle.className = 'products__title';
+  productPhoto.className = 'products__photo';
+  productImg.className = 'products__img';
+  productPrice.className = 'products__price';
+  productCur.className = 'products__currency';
+  productOptions.className = 'products__options';
+  productDetails.className = 'products__details';
+  productCart.className = 'products__cart';
 
   products.append(productItem);
   productItem.append(productTitle);
@@ -51,17 +48,17 @@ function itemMaker(id) {
 
   productImg.src = data[id].images[0] ? data[id].images[0] : data[id].images;
   productTitle.innerHTML = data[id].title;
-  productCur.innerHTML = "$";
+  productCur.innerHTML = '$';
   productPrice.innerHTML = `${data[id].price} ${productCur.innerHTML}`;
-  productDetails.innerHTML = "Details";
-  productCart.innerHTML = "Add To Cart";
+  productDetails.innerHTML = 'Details';
+  productCart.innerHTML = 'Add To Cart';
 }
 
 function mkProducts() {
-  for (let id = 1; id < numberOfProducts; id++) {
+  for (let id = 1; id < PRODUCTS_NUMBER; id++) {
     itemMaker(id);
   }
 }
 
 //adding to cart
-let productToCart = document.querySelectorAll(".products__cart");
+let productToCart = document.querySelectorAll('.products__cart');
